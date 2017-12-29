f7e3bd50a2da488442c4bd8af59b3f498cab10aa
ohr486 <ohr486@gmail.com>
2017-12-29 22:46:41 +0900

    update kafka

--- lib/phoenix_pubsub_kafka/kafka.ex
+++ lib/phoenix_pubsub_kafka/kafka.ex
@@ -3,24 +3,32 @@ defmodule Phoenix.PubSub.Kafka do
 
   def start_link(name, opts) do
     sup_name = Module.concat(name, Supervisor)
-    Supervisor.start_link(__MODULE__, [name, opts], name: sup_name)
+    Supervisor.start_link(__MODULE__, {name, opts}, name: sup_name)
   end
 
-  def init([server_name, opts]) when is_atom(server_name) do
+  def init({server_name, opts}) when is_atom(server_name) do
     local_name = Module.concat(server_name, Local)
     node_name = self()
     server_opts = []
-    pool_size = 5
+
+    fastlane = opts[:fastlane]
+    pool_size = 1
+    node_ref = :crypto.strong_rand_bytes(24)
 
     dispatch_rules = [
-      {:broadcast, Phoenix.PubSub.KafkaServer, []},
-      {:direct_broadcast, Phoenix.PubSub.KafkaServer, []},
+      {:broadcast, Phoenix.PubSub.Kafka.Producer.Server, [fastlane, pool_size, node_ref]},
+      {:direct_broadcast, Phoenix.PubSub.Kafka.Producer.Server, []},
       {:node_name, __MODULE__, [node_name]}
     ]
 
+    IO.puts "-------------------"
+    IO.inspect server_name
+    IO.puts "-------------------"
+
     children = [
       supervisor(Phoenix.PubSub.LocalSupervisor, [server_name, pool_size, dispatch_rules]),
-      worker(Phoenix.PubSub.KafkaServer, [server_opts])
+      supervisor(Phoenix.PubSub.Kafka.Producer.Supervisor, [server_opts]),
+      supervisor(Phoenix.PubSub.Kafka.Consumer.Supervisor, [server_opts])
     ]
 
     supervise(children, strategy: :one_for_all)
