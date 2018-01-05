e168f76f4e297374b065d2718c9d3447c1905d93
ohr486 <ohr486@gmail.com>
2018-01-05 21:27:21 +0900

    update mix

--- mix.exs
+++ mix.exs
@@ -9,6 +9,17 @@ defmodule PhoenixPubsubKafka.Mixfile do
       version: @version,
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
+      dialyzer: [
+        plt_add_deps: :transitive,
+        flags: [
+          :error_handling,
+          :race_connditions
+        ]
+      ],
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [coveralls: :test],
+      description: description(),
+      package: package(),
       deps: deps()
     ]
   end
@@ -23,7 +34,23 @@ defmodule PhoenixPubsubKafka.Mixfile do
     [
       {:phoenix_pubsub, "~> 1.0"},
       {:kafka_ex, "~> 0.8.1"},
+      {:credo, "~> 0.8.10", only: :dev},
+      {:dialyxir, "~> 0.5.1", only: :dev},
+      {:excoveralls, "~> 0.8", only: :test},
       {:ex_doc, "~> 0.17.1", only: :docs}
+    ]
+  end
+
+  defp description do
+    "Kafka adapter for Phoenix's PubSub layer."
+  end
+
+  defp package do
+    [
+      maintainers: ["ohr486"],
+      files: ["lib", "config/config.exs", "mix.exs", "README.md"],
+      licenses: ["MIT"],
+      links: %{"Github" => "https://github.com/ohr486/phoenix_pubsub_kafka"}
     ]
   end
 end
