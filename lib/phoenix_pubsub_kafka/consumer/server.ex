3e4daef81105378dc110c4123a2681a1c278b65f
ohr486 <ohr486@gmail.com>
2018-01-15 20:38:30 +0900

    ignore debug cons info

--- lib/phoenix_pubsub_kafka/consumer/server.ex
+++ lib/phoenix_pubsub_kafka/consumer/server.ex
@@ -6,19 +6,19 @@ defmodule Phoenix.PubSub.Kafka.Consumer.Server do
   alias Phoenix.PubSub.Kafka.Config
 
   def handle_message_set(message_set, state) do
-    Logger.debug "### Phoenix.PubSub.Kafka.Consumer.Server.hanndle_message_set(#{inspect message_set}, #{inspect state}) ###"
+    #Logger.debug "### Phoenix.PubSub.Kafka.Consumer.Server.hanndle_message_set(#{inspect message_set}, #{inspect state}) ###"
     for %Message{value: message} <- message_set do
-      Logger.debug(fn -> "message: " <> inspect(message) end)
+      #Logger.debug(fn -> "message: " <> inspect(message) end)
 
       {remote_node_ref, fastlane, pool_size, from_pid, phx_topic, msg} = Config.serializer.decode_message(message)
-      Logger.debug "-------------------------------"
-      Logger.debug "# remote_node_ref: #{inspect remote_node_ref} #"
-      Logger.debug "# fastlane: #{inspect fastlane} #"
-      Logger.debug "# pool_size: #{inspect pool_size} #"
-      Logger.debug "# from_pid: #{inspect from_pid} #"
-      Logger.debug "# phx_topic: #{inspect phx_topic} #"
-      Logger.debug "# msg: #{inspect msg} #"
-      Logger.debug "-------------------------------"
+      #Logger.debug "-------------------------------"
+      #Logger.debug "# remote_node_ref: #{inspect remote_node_ref} #"
+      #Logger.debug "# fastlane: #{inspect fastlane} #"
+      #Logger.debug "# pool_size: #{inspect pool_size} #"
+      #Logger.debug "# from_pid: #{inspect from_pid} #"
+      #Logger.debug "# phx_topic: #{inspect phx_topic} #"
+      #Logger.debug "# msg: #{inspect msg} #"
+      #Logger.debug "-------------------------------"
 
       Local.broadcast(fastlane, ChatBackend.PubSub, 1, from_pid, phx_topic, msg)
     end
