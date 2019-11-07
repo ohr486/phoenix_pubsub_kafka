7258999f979cc9158680575bf59bf450257ef033
ohr486 <ohr486@gmail.com>
2019-11-07 20:57:08 +0900

    add moduledoc

--- lib/phoenix_pubsub_kafka/serializer.ex
+++ lib/phoenix_pubsub_kafka/serializer.ex
@@ -2,6 +2,8 @@ defmodule Phoenix.PubSub.Kafka.Serializer do
   @moduledoc nil
 
   defmodule Binary do
+    @moduledoc nil
+
     def encode_message(message) do
       :erlang.term_to_binary(message)
     end
